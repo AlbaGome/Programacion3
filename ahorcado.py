3cbc1b68938e8da25dba33e8bea7c8963e09af3b
AlbaGome <albaliriagomezmontano@gmail.com>
2023-08-22 17:59:11 -0400

    version 2023-08-22_02

--- ahorcado.py
+++ ahorcado.py
@@ -1,6 +1,5 @@
-def jugar_ahorcado():
-    print('=================================')
-    print('Bienvenido al Juego de la Horca')
-    print('=================================')
-    
-    print('Fin del Juego')
+print('================================')
+print('Bienvenido al Juego del Ahorcado')
+print('================================')
+
+print('Fin del Juego')
