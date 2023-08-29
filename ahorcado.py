75f8c737abbe70723b9dc3a2486838f8810fc80f
AlbaGome <albaliriagomezmontano@gmail.com>
2023-08-29 18:12:53 -0400

    version 2023-08-29_02

--- ahorcado.py
+++ ahorcado.py
@@ -2,6 +2,14 @@ def jugar():
   print('================================')
   print('Bienvenido al Juego del Ahorcado')
   print('================================')
+  
+  palabra_secreta = 'mandarina'
+
+  ahorcado = false
+  acerto = false
+
+  while(not ahorcado and not acerto):
+        print("jugando...")
 
   print("Fin del Juego")
     
